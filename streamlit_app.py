065c013ca902a4f5b01de1651b009e63015a5810
Michal Michalski <mihum@poczta.onet.pl>
2026-07-09 01:44:44 +0200

    Fix syntax error in dataframe assignment
    Fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ st.write("The name on your Smoothie will be:", name_on_order)
 # session = get_active_session()
 cnx = st.connection('snowflake')
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('Fruit_name'), col('Search_on')
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('Fruit_name'), col('Search_on'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
 # st.stop()
                                                                       
