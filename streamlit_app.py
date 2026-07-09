09b7dba10ab151f68bb5660a4ff00df96aa0968f
Michal Michalski <mihum@poczta.onet.pl>
2026-07-09 04:02:28 +0200

    Comment out ingredients string stripping
    Commented out the line that strips the ingredients string.

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,7 +36,7 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:  
         ingredients_string += fruit_chosen + ' '
         # ingredients_string += fruit_chosen + ' '
-        ingredients_string = ingredients_string.strip()
+        # ingredients_string = ingredients_string.strip()
 
         
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
