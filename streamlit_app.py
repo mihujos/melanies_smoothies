74c813eb5ed1d4acb6e3794e6bdd8852234ee18c
Michal Michalski <mihum@poczta.onet.pl>
2026-07-09 02:55:44 +0200

    Update API endpoint URLs in streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,11 +42,11 @@ if ingredients_list:
 
       
         st.subheader = (fruit_chosen + ' Nutrition Information')
-        smoothiefroot_response = requests.get(f"https://smoothiefroot.com/api/fruit/{search_on}")  
+        smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{search_on}")  
         # st.text(smoothiefroot_response.json())
         sf_df = st.dataframe (data = smoothiefroot_response.json(), use_container_width = True)
 
-        # fruityvice_response = requests.get("https:/smoothiefroot.com/api/fruit/{fruit_chosen}") 
+        # fruityvice_response = requests.get("https:/my.smoothiefroot.com/api/fruit/{fruit_chosen}") 
         # sf_df = st.dataframe (data = fruityvice_response.json(), use_container_width = True)
       
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
