a9c90c3d77758617f7a3a287aef5f905498524b8
Michal Michalski <mihum@poczta.onet.pl>
2026-07-09 02:08:48 +0200

    Refactor API calls and dataframe for fruit info
    Updated API request handling and dataframe display for fruit nutrition information.

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,12 +42,12 @@ if ingredients_list:
 
       
         st.subheader = (fruit_chosen + ' Nutrition Information')
-        # smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)  
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/{fruit_chosen}")  
         # st.text(smoothiefroot_response.json())
-        # sf_df = st.dataframe (data = smoothiefroot_response.json(), use_container_width = True)
+        sf_df = st.dataframe (data = smoothiefroot_response.json(), use_container_width = True)
 
-        fruityvice_response = requests.get("https://my.smoothiefroot.com/api/fruit/{fruit_chosen}") 
-        sf_df = st.dataframe (data = fruityvice_response.json(), use_container_width = True)
+        # fruityvice_response = requests.get("https://my.smoothiefroot.com/api/fruit/{fruit_chosen}") 
+        # sf_df = st.dataframe (data = fruityvice_response.json(), use_container_width = True)
       
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
                         values ('""" + ingredients_string + """', '""" + name_on_order + """ ')"""
